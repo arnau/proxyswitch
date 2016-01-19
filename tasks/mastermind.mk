6e129eb4a917b94cc893419131b0073d6bd5f403
Arnau Siches <arnau@ustwo.com>
2016-01-19 12:37:58 +0000

    Add schema validation with STDOUT raw result

--- tasks/mastermind.mk
+++ tasks/mastermind.mk
@@ -18,6 +18,12 @@ mastermind-script:
 
 mastermind-driver:
 	@$(shell pwd)/mastermind.py --quiet \
+                              --without-proxy-settings \
+                              --with-driver \
+                              --source-dir $(shell pwd)/test/records
+
+mastermind-driver-system:
+	@$(shell pwd)/mastermind.py --quiet \
                               --with-driver \
                               --source-dir $(shell pwd)/test/records
 .PHONY: mastermind-driver
@@ -57,3 +63,15 @@ schematics-driver-state:
               arnau/mermaid mermaid --png \
                                     -o schematics/ \
                                     schematics/driver-stateful.mmd
+
+test-api-call:
+	@curl -ki \
+        --proxy http://localhost:8080 \
+        -XGET https://api.github.com/users/octocat/orgs
+
+test-api-call2:
+	@curl -L --proxy http://localhost:8080 \
+        -XGET http://proxapp:5000/fake/start/
+	@curl -ki \
+        --proxy http://localhost:8080 \
+        -XGET https://api.github.com/users/arnau/orgs
